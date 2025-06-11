c8d4e6cf3210fd2c56c3e9e554c8fb3015641511
prince7982 <princekumar13810@gmail.com>
2025-06-11 12:36:05 +0530

    sidebar add

--- src/index.js
+++ src/index.js
@@ -8,16 +8,29 @@ import Loginpage from './modules/auth/Loginpage';
 import Registerpage from './modules/auth/Registerpage';
 import Errorpage from './modules/shares/Errorpage';
 import MyEffect from './MyEffect';
+import Welcomepage from './modules/dashboard/Welcomepage';
+import Profilepage from './modules/dashboard/Profilepage';
+import Mainpage from './modules/dashboard/Mainpage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='' element={<Loginpage/>}></Route>
-        <Route path='register' element={<Registerpage/>}></Route>
-        <Route path='*' element={<Errorpage/>}></Route>
-        <Route path='myeffect' element={<MyEffect/>}></Route>
+        <Route path='' element={<Loginpage />}></Route>
+        <Route path='register' element={<Registerpage />}></Route>
+        <Route path='dashboard' element={<Welcomepage />}></Route>
+        <Route path='dashboard/profile' element={<Profilepage />}>
+          <Route path='' element={<Mainpage />}></Route>
+          <Route path='price' element={<Mainpage />}></Route>
+
+          <Route path='*' element={<Errorpage />}></Route>
+
+        </Route>
+
+        
+        <Route path='*' element={<Errorpage />}></Route>
+        <Route path='myeffect' element={<MyEffect />}></Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
---
+++ src/modules/shares/Myappmenu.js
@@ -0,0 +1,27 @@
+export const myapplist = [
+    {
+        appname:"profile",
+        approut:"profile",
+        appicons:""
+    },
+    {
+        appname:"Balance",
+        approut:"balance",
+        appicons:""
+    },
+    {
+        appname:"Sales",
+        approut:"salespage",
+        appicons:""
+    },
+    {
+        appname:"profile",
+        approut:"profile",
+        appicons:""
+    },
+    {
+        appname:"profile",
+        approut:"profile",
+        appicons:""
+    }
+];
