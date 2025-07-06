b59344718d2bdb1f3c5d56b4066e336a3c4f98d5
prince7982 <princekumar13810@gmail.com>
2025-07-06 19:01:17 +0530

    apichange

--- src/modules/dashboard/components/Productlistpage.js
+++ src/modules/dashboard/components/Productlistpage.js
@@ -1,17 +1,27 @@
 import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom';
 
 function Productlistpage() {
     const [mydata, setdata] = useState([])
 
-    const Myapi = () => {
-        fetch('https://dummyjson.com/products').then((res) => {
-            //   console.log(res.json());
-            return res.json()
-        }).then((d) => {
-            console.log(d);
-            setdata(d.products)
+    // const Myapi = () => {
+    //     fetch('https://dummyjson.com/products').then((res) => {
+    //         console.log(res);
+    //         return res.json()
+    //     }).then((d) => {
+    //         console.log(d);
+    //         setdata(d.products)
+    //     })
+    // }
+
+    const Myapi = ()=>{
+        axios.get('https://dummyjson.com/products').then((d)=>{
+            console.log(d.data.products);
+            setdata(d.data.products);
         })
     }
+
 
     useEffect(()=>{
         Myapi();
@@ -26,6 +36,7 @@ function Productlistpage() {
                         <div className='card p-3'>
                             <h5>Pro Id:{d.id}</h5>
                             <img src={d.thumbnail}/>
+                            <Link to={`detailspage/`+d.id} className='btn btn-primary'>Product Details</Link>
                         </div>
                     </div>)
                 })}
